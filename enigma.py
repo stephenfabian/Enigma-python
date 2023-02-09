e9f69b52a584050e7d751645ace45cd2a6024ece
Stephen Fabian <fabianstephen@gmail.com>
2023-02-08 23:14:09 -0700

    adds classes, corresponding spec files

--- enigma.py
+++ enigma.py
@@ -1,9 +1,16 @@
-def default_key():
-    import random
-    random_number = random.randint(00000, 99999)
-    return random_number
-    # import ipdb; ipdb.set_trace()
+class Enigma:
+    def default_key(self):
+        import random
+        random_number = random.randint(00000, 99999)
+        return random_number
 
-def default_date():
-    import datetiem
-    today = datetime.datetime.now().strftime("%d%m%y")
+    def default_date(self):
+        import datetime
+        todays_date = int(datetime.datetime.now().strftime("%d%m%y"))
+        # import ipdb; ipdb.set_trace()
+        return todays_date
+
+    # def encrypt(message, key=default_key, date=default_date):
+   
+    # def decrypt(ciphertext, key=default_key, date=default_date):
+
---
+++ letter.py
@@ -0,0 +1,13 @@
+class Letter:
+  def __init__(letter, shift):
+    letter = letter.lower()
+
+  def alphabet(self):
+    import string
+    return list(string.ascii_lowercase)
+
+  def encrypt_letter():
+    alphabet.index(letter % len(alphabet))
+
+  
+  # def decrypt_letter():
